5d5e1b3d53c6a26c0bc6b3121ec179ffc852c9a7
Rubi <rubi@ekak.in>
2023-07-22 12:31:22 +0530

    changes heading color

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import "./App.css";  
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Form from "./Component/Form";
 import Mainpage from "./Component/Mainpage";
 import SuccessMessage from "./Component/SuccessMessage";
 import Ekaksharform from "./Component/Ekaksharform";
-
 
 // import Multileveldropdown from "./Component/Multileveldropdown";
 // import Aboutservice from './Component/Aboutservice';
@@ -16,21 +15,18 @@ function App() {
   return (
     <div className="">
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Mainpage/>}></Route>
-        <Route path="/Ekakshar-product-form" element={<Ekaksharform/>}></Route>
-        <Route path="/Ekakshar-product-success" element={<SuccessMessage/>}></Route>
-        
-       
-      </Routes>
-      
-      
+        <Routes>
+          <Route path="/" element={<Mainpage />}></Route>
+          <Route
+            path="/Ekakshar-product-form"
+            element={<Ekaksharform />}
+          ></Route>
+          <Route
+            path="/Ekakshar-product-success"
+            element={<SuccessMessage />}
+          ></Route>
+        </Routes>
       </BrowserRouter>
-
-      
-      
-
-    
     </div>
   );
 }
